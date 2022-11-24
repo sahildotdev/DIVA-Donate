8838a9dd777efe2bcf58ff1081ca56f08c62c009
Walodja1987 <wladimir.weinbender@gmail.com>
2022-11-24 12:15:17 +0100

    example setup

---
+++ src/components/Logo.tsx
@@ -0,0 +1,7 @@
+export const Logo = () => {
+  return (
+    <span className="text-lg">
+      Super<span className="font-bold">Defi</span>
+    </span>
+  );
+};
---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import { Logo } from "../components/Logo";
+
+export default function Home() {
+  return (
+    <main className="h-full w-full relative bg-blue-200 ">
+      <nav>
+        <Logo />
+      </nav>
+    </main>
+  );
+}
