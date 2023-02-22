731fa6a4d5076feeb45cef9cc23b20fbf058a641
macroman321 <aleksandar.coha@gmail.com>
2023-02-22 09:05:32 +0100

    addressed comments

--- src/components/Section/CampaingCard.tsx
+++ src/components/Section/CampaingCard.tsx
@@ -257,9 +257,9 @@ export const CampaingCard = () => {
                 <div className="flex flex-row justify-between border-spacing-x-8">
                   {approveLoading ? (
 
-                      <div role="status">
+                      <div style={{width:'50%'}} role="status">
                         <svg aria-hidden="true"
-                             className="w-9 h-9 ml-20 mt-10 mr-10 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+                             className="w-9 h-9 ml-20 mt-11 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
                              viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
                           <path
                               d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
@@ -272,14 +272,14 @@ export const CampaingCard = () => {
                       </div>
 
                   ) : (<button id='approve' onClick={handleApprove}
-                           className="disabled:opacity-25 w-full mt-10 mr-3 py-3 text-lg text-white bg-[#042940] rounded-[10px] hover:bg-[#042940] focus:outline-none focus:ring-2 focus:ring-[#005C53] focus:ring-opacity-50"
-                           type="button" disabled={!approveEnabled}>
+                           className="w-full disabled:opacity-25 mt-10 mr-1 py-3 text-lg text-white bg-[#042940] rounded-[10px] hover:bg-[#042940] focus:outline-none focus:ring-2 focus:ring-[#005C53] focus:ring-opacity-50"
+                           type="button" style={{width:'50%'}} disabled={!approveEnabled}>
                     Approve
                   </button>)}
                   {donateLoading ? (
-                      <div classname="absolute w-full" role="status">
+                      <div style={{width:'50%'}} role="status">
                         <svg aria-hidden="true"
-                             className="w-9 h-9 ml-10 mt-10 mr-12 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+                             className="w-9 h-9 ml-20 mt-11 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
                              viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
                           <path
                               d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
@@ -292,8 +292,8 @@ export const CampaingCard = () => {
                       </div>
                   ):(
                       <button id='donate' onClick={handleDonation}
-                           className="disabled:opacity-25 w-full mt-10 py-3 text-lg text-white bg-[#042940] rounded-[10px] hover:bg-[#042940] focus:outline-none focus:ring-2 focus:ring-[#005C53] focus:ring-opacity-50"
-                           type="button" disabled={!donateEnabled}>
+                           className="w-full disabled:opacity-25 mt-10 py-3 text-lg text-white bg-[#042940] rounded-[10px] hover:bg-[#042940] focus:outline-none focus:ring-2 focus:ring-[#005C53] focus:ring-opacity-50"
+                           type="button" style={{width:'50%'}} disabled={!donateEnabled}>
                         Donate
                       </button>
                   )}
