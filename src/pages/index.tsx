c400df3acd74ab87d1bdef1ec95938969b942e61
macroman321 <aleksandar.coha@gmail.com>
2023-03-01 08:56:13 +0100

    mobile placeholder

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,8 +9,8 @@ import {FooterSection} from "../components/Section/FooterSection";
 
 export default function Home() {
   return (
-    <main className="h-full w-full relative">
-          <div className="invisible sm:visible sm:">
+    <main className="sm:h-full sm:w-full sm:relative">
+          <div className="invisible hidden sm:block sm:visible">
               <Layout>
                 <HeroSection />
                 <InfoSection />
@@ -19,13 +19,13 @@ export default function Home() {
                 <WorkSection />
               </Layout>
           </div>
-          <div className="justify-center items-center -mt-[320rem] items-center sm:invisible sm:hidden">
+          <div className="pt-[30rem] sm:invisible sm:hidden">
               <InfoCard
                   cardPadding="p-4 md:p-8"
                   cardWidth="w-auto md:w-full"
                   cardRadius="20px"
-                  title="In progress..."
-                  paragraph="Mobile support is currently in progress. Please check back soon!"
+                  title="Mobile is currently not supported. Please check on a desktop computer."
+                  paragraph=""
                   paragraphColor="text-[#042940]"
                   titleSize="text-[23px] md:text-[46px]"
                   paragraphSize="text-sm md:text-base"
